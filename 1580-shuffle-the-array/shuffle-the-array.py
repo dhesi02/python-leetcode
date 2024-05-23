435ada9fd3cd2c8a710e60fab1555397da23fedd
Dhesigan <167281009+dhesi02@users.noreply.github.com>
2024-05-23 09:14:49 +0530

    Time: 39 ms (37.19%) | Memory: 11.8 MB (78.72%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def shuffle(self, nums, n):
-        """
-        :type nums: List[int]
-        :type n: int
-        :rtype: List[int]
-        """
         ans=[]
         for i,j in zip(nums[:n],nums[n:]):
             ans+=[i,j]
